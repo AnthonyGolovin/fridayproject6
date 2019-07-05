2b4b94c90abc5f524587b74b08e87b1c4627c100
AnthonyGolovin <AGOLOVAIN97@live.com>
2019-07-05 10:22:39 -0700

    Trying to fix windows run start issues

--- src/main.js
+++ src/main.js
@@ -6,13 +6,15 @@ import './styles.css';
 
 $(document).ready(function() {
 
-  $('#search').click(function() {
-     const city = $('#data').val();
-     $('#data').val("");
+  $('#searchDoctor').click(function() {
+     let city = $('#dataDoctorInput').val();
+     let city2 = $('#dataSicknessInput').val();
+
+     console.log(city);
+     console.log(city2);
 
       let request = new XMLHttpRequest();
       const url = 'https://api.betterdoctor.com/2016-03-01/doctors?name=mame&query=wuery&location=Portland&skip=0&limit=10&user_key=3dafcc36df77a6c43a3df6522adae0f4';
-
 
       request.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
@@ -24,4 +26,21 @@ $(document).ready(function() {
    }
  }
 });
+
+// $('#searchSickness').click(function() {
+//    const city = $('#dataSickness').val();
+//    $('#dataSickness').val("");
+//
+//    let request = new XMLHttpRequest();
+//    const url = 'https://api.betterdoctor.com/2016-03-01/doctors?name=mame&query=wuery&location=Portland&skip=0&limit=10&user_key=3dafcc36df77a6c43a3df6522adae0f4';
+//
+//    request.onreadystatechange = function() {
+//      if (this.readyState === 4 && this.status === 200) {
+//        const response = JSON.parse(this.responseText);
+//        getElements(response);
+//
+// request.open("GET", url, true);
+// request.send();
+// }
+// }
 });

f74e0a1aa1d2b0568fd4e0ca32cc4b2b8480f370
AnthonyGolovin <AGOLOVAIN97@live.com>
2019-07-05 11:23:13 -0700

    Reform of function, and split front end and back end logic

--- src/fridayproject6.js
+++ src/fridayproject6.js
@@ -1,1 +1,23 @@
 //BackEnd
+
+function getMedicalInfo(doctor,sickness) {
+
+  let answer;
+  let request = new XMLHttpRequest();
+  const url = 'https://api.betterdoctor.com/2016-03-01/doctors?name=' + doctor + '&query=' + sickness + '&location=45.5155,122.6793&skip=0&limit=10&user_key=3dafcc36df77a6c43a3df6522adae0f4';
+
+  request.open("GET", url, true);
+
+  request.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      getElements(response);
+    }
+  }
+  const getElements = function(response) {
+    answer = response.meta.data_type;
+  }
+  request.send();
+  return answer;
+ }
+ exports.medic = getMedicalInfo;
--- src/main.js
+++ src/main.js
@@ -3,28 +3,19 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { FuckThisBullShit } from './fridayproject6.js';
 
 $(document).ready(function() {
 
   $('#searchDoctor').click(function() {
-     let city = $('#dataDoctorInput').val();
-     let city2 = $('#dataSicknessInput').val();
+     let doctor = $('#dataDoctorInput').val();
+     let sickness = $('#dataSicknessInput').val();
 
-     console.log(city);
-     console.log(city2);
+     console.log(doctor);
+     console.log(sickness);
 
-      let request = new XMLHttpRequest();
-      const url = 'https://api.betterdoctor.com/2016-03-01/doctors?name=mame&query=wuery&location=Portland&skip=0&limit=10&user_key=3dafcc36df77a6c43a3df6522adae0f4';
 
-      request.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
-          const response = JSON.parse(this.responseText);
-          getElements(response);
-
-  request.open("GET", url, true);
-  request.send();
-   }
- }
+         
 });
 
 // $('#searchSickness').click(function() {

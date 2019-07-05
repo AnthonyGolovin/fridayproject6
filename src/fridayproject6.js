8fc66ccadfd6a479cff21d83728cfe085c1f9513
AnthonyGolovin <AGOLOVAIN97@live.com>
2019-07-05 13:44:57 -0700

    Add front end search results

--- src/fridayproject6.js
+++ src/fridayproject6.js
@@ -2,7 +2,7 @@
 
 function getMedicalInfo(doctor,sickness) {
 
-  let answer;
+  let answer = [];
   let request = new XMLHttpRequest();
   const url = 'https://api.betterdoctor.com/2016-03-01/doctors?name=' + doctor + '&query=' + sickness + '&location=or-portland&skip=0&limit=10&user_key=3dafcc36df77a6c43a3df6522adae0f4';
 
@@ -17,11 +17,15 @@ function getMedicalInfo(doctor,sickness) {
     }
   };
   const getElements = function(response) {
-    answer = response.data;
+    for (let i = 0; i < response.meta.count; i++){
+      answer.push(response.data[i].practices[0].name);
+}
     console.log(answer);
   };
   request.send();
   console.log(answer);
   return answer;
+
  }
+
  exports.medic = getMedicalInfo;
--- src/main.js
+++ src/main.js
@@ -16,23 +16,8 @@ $(document).ready(function() {
      console.log(sickness);
      console.log(medic(doctor,sickness));
 
+     $('showInfo').append(getElements);
 
-});
+   });
 
-// $('#searchSickness').click(function() {
-//    const city = $('#dataSickness').val();
-//    $('#dataSickness').val("");
-//
-//    let request = new XMLHttpRequest();
-//    const url = 'https://api.betterdoctor.com/2016-03-01/doctors?name=mame&query=wuery&location=Portland&skip=0&limit=10&user_key=3dafcc36df77a6c43a3df6522adae0f4';
-//
-//    request.onreadystatechange = function() {
-//      if (this.readyState === 4 && this.status === 200) {
-//        const response = JSON.parse(this.responseText);
-//        getElements(response);
-//
-// request.open("GET", url, true);
-// request.send();
-// }
-// }
 });

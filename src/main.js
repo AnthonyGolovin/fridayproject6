232304c706bbe863670322ddbe13501ad938427c
AnthonyGolovin <AGOLOVAIN97@live.com>
2019-07-05 09:37:36 -0700

    Added docotr Api url

--- src/main.js
+++ src/main.js
@@ -11,5 +11,17 @@ $(document).ready(function() {
      $('#data').val("");
 
       let request = new XMLHttpRequest();
-      const url =;
+      const url = 'https://api.betterdoctor.com/2016-03-01/doctors?name=mame&query=wuery&location=Portland&skip=0&limit=10&user_key=3dafcc36df77a6c43a3df6522adae0f4';
+
+
+      request.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+          const response = JSON.parse(this.responseText);
+          getElements(response);
+
+  request.open("GET", url, true);
+  request.send();
+   }
+ }
+});
 });
